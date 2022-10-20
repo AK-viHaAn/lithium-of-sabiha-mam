dc79275669c82b17953a9e5da0f6ad503c2ea949
AK-viHaAn <kanand2607755@gmail.com>
2022-10-21 01:41:33 +0530

    players wala solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,40 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+let players =
+    [{
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": ["swimming"]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": ["soccer"]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": ["soccer"]
+    }]
+
+router.post("/players", function (req, res) {
+
+    let userData = req.body
+
+    let playerName = players.find( x => x.name === userData.name)
+
+    if (playerName) {
+        res.send({ warning: "Player name already exist !!"})
+    } else {
+        players.push(userData)
+        res.send(players)
+    }
+});
 module.exports = router;

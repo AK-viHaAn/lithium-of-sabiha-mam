6d286f199422b22ca9fa1885377de40adf74a7c4
AK-viHaAn <kanand2607755@gmail.com>
2022-11-07 10:13:27 +0530

    assignment of auth2 date 04/11

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://viHaAn:vihaan@cluster0.tznueha.mongodb.net/JsonWebToken", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
